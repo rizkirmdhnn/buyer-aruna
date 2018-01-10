12e724d748508a7a6dc895a501486b5a44b9e7f5
Ren <rendy1429@gmail.com>
2018-01-10 12:01:36 +0700

    fix navigator

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@
  */
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { TabNavigator } from 'react-navigation';
-
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 /**
  *  List Page
@@ -13,31 +12,68 @@ import MasterPage from './pages/MasterPage';
 import LoginFormPage from './pages/LoginFormPage';
 import RequestOrderPage from './pages/RequestOrderPage';
 import HomePage from './pages/HomePage';
-import TransactionPage from './pages/TransactionPage'
+import TransactionPage from './pages/TransactionPage';
+import RequestFormOrderPage from './pages/RequestFormOrderPage';
+import ProfileSupplierPage from './pages/ProfileSupplierPage';
+/**
+ *  List Component
+ */
+import { HeaderHome } from './components/common';
 
 
 class App extends React.Component {
-    state = { loggedIn: null };
+    state = { loggedIn: false };
 
     renderContent() {
         switch (this.state.loggedIn) {
             case false:
                 return <LoginFormPage />;
             default:
-                return <MasterPage />;
+                return <MainNavigator />;
         }
     }
 
     render() {
-        const MainNavigator = TabNavigator ({
-            home: { screen: HomePage },
-            request: { screen: RequestOrderPage },
-            transaction: { screen: TransactionPage }
-        });
+        const MainNavigator = TabNavigator({            
+            Home: { screen: HomePage },
+            Request: { screen: RequestOrderPage },
+            Transaction: {
+                screen: StackNavigator({
+                    Transaction: { screen: TransactionPage },
+                    Login: {
+                        screen: LoginFormPage,
+                        navigationOptions: {
+                            title: 'Login',
+                            header: null,
+                            tabBarVisible: false
+                        },
+                    },
+                    RequestFormOrder: {
+                        screen: RequestFormOrderPage,
+                        navigationOptions: {
+                            title: 'RequestFormOrder',
+                            header: null,
+                            tabBarVisible: false
+                        },
+                    },
+                    ProfileSupplier: {
+                        screen: ProfileSupplierPage,
+                        navigationOptions: {
+                            title: 'ProfileSupplier',
+                            // header: null,
+                            tabBarVisible: false
+                        },
+                    },
+                })
+            },
+        },
+    );
 
         return (
-            <View style={styles.container}>
-                <MainNavigator />
+            <View style={styles.container} >
+                {/* <HeaderHome headerText="Home Aruna" /> */}
+                <HeaderHome />
+                <MainNavigator style={styles.tabNav} />
             </View>
         );
     };
@@ -50,7 +86,7 @@ const styles = {
         backgroundColor: '#FFF'
     },
     tabNav: {
-        backgroundColor: '#FFF'
+        backgroundColor: 'red'
     }
 }
 
