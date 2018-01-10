485def9c25815c8a0d55e3b3cebc6d21d8e5aa68
Ren <rendy1429@gmail.com>
2018-01-10 12:01:11 +0700

    add new page and components

---
+++ src/pages/ProfileSupplierPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class ProfileSupplierPage extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text> Profile Supplier Page </Text>
+            </View>
+        );
+    }
+};
+
+export default ProfileSupplierPage;
---
+++ src/pages/RequestFormOrderPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class RequestFormOrderPage extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text> Request Form Order </Text>
+            </View>
+        );
+    }
+};
+
+export default RequestFormOrderPage;
