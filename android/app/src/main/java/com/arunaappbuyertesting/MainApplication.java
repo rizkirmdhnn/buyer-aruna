343db3da48e2bb167217a846a2ac0c0bcd7f5e51
Raosan <raosanf.lillahi@gmail.com>
2018-02-22 11:16:00 +0700

    CodePush configuration

--- android/app/src/main/java/com/arunaappbuyertesting/MainApplication.java
+++ android/app/src/main/java/com/arunaappbuyertesting/MainApplication.java
@@ -3,6 +3,7 @@ package com.arunaappbuyertesting;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -18,6 +19,12 @@ import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;  // ADD THIS
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -27,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new VectorIconsPackage(),
               new ImagePickerPackage(),
                 new ReactNativeOneSignalPackage() 
