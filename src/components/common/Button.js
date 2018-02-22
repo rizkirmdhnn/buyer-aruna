fd355adbbd8f7eed4591feb11d1aeb9a3262c466
Raosan <raosanf.lillahi@gmail.com>
2018-02-22 12:37:48 +0700

    font

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import { COLOR } from './../../shared/lb.config';
+import { COLOR } from '../../shared/lb.config';
 
 const Button = ({onPress, children}) => {
-    
-    const { buttonStyle, textStyle } = styles;
+  
+  const { buttonStyle, textStyle } = styles;
 
-    return (
-        <TouchableOpacity onPress={onPress} style={buttonStyle}>
-            <Text style={textStyle}>{children}</Text>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>{children}</Text>
+    </TouchableOpacity>
+  );
 };
 
 
 const styles = {
-    textStyle: {
-        alignSelf: 'center',
-        color: '#FFF',
-        fontSize: 16,
-        fontWeight: '600',
-        paddingTop: 10,
-        paddingBottom: 10
-    },
-    buttonStyle: {
-        flex: 1,
-        alignSelf: 'stretch',
-        backgroundColor: COLOR.secondary_a,
-        borderRadius: 5,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 5
-    }
+  textStyle: {
+    alignSelf: 'center',
+    color: '#FFF',
+    fontSize: 16,
+    paddingTop: 12,
+    paddingBottom: 12,
+    fontFamily: 'Muli-Bold'
+
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: COLOR.primary,
+    borderRadius: 8,
+  }
 };
 
 export { Button };
