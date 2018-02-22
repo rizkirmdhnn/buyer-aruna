ffbeb53b773220318a13b870512bf72cfa5d7a31
Raosan <raosanf.lillahi@gmail.com>
2018-02-22 21:30:31 +0700

    Card

--- src/pages/RequestOrderPage.js
+++ src/pages/RequestOrderPage.js
@@ -4,227 +4,228 @@ import { Header, SearchBar, Icon } from 'react-native-elements';
 import { BASE_URL } from './../shared/lb.config';
 import axios from 'axios';
 import {
-    CardRegistration,
-    CardSectionRegistration,
-    InputRegistration,
-    Button,
-    ContainerSection,
-    Container,
-    Spinner,
-    Card
+  CardRegistration,
+  CardSectionRegistration,
+  InputRegistration,
+  Button,
+  ContainerSection,
+  Container,
+  Spinner,
+  Card
 } from './../components/common';
 import moment from 'moment';
 
 class RequestOrderPage extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            tokenUser: '',
-            dataReqOrder: '',
-            expiredContainer: null,
-            NoExpiredContainer: null
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      tokenUser: '',
+      dataReqOrder: '',
+      expiredContainer: null,
+      NoExpiredContainer: null
     };
+  };
 
-    async componentWillMount() {
-        try {
-            const value = await AsyncStorage.getItem('loginCredential');
-            if (value !== null) {
-                console.log(value, 'Storage Request');
-                this.setState({ tokenUser: value })
-                return this.getData();
-            }
-        } catch (error) {
-            console.log(error, 'Error Storage Request');
-        }
-    }
-
-    getData() {
-        axios.get(`${BASE_URL}/buyer/requests?key=to&page=0&pageSize=30&sorting=desc`, {
-            headers: {
-                'token': this.state.tokenUser
-            }
-        }).then(response => {
-            res = response.data.data;
-            console.log(res, 'Data Request Order');
-            this.setState({ dataReqOrder: res, loading: false });
-        })
-            .catch(error => {
-                this.setState({ loading: false });
-                console.log(error.response, 'Erroor nya');
-                console.log('Error Request Order Get Data');
-            })
-    }
-
-    static navigationOptions = {
-        title: 'Permintaan',
-        headerRight: <View />
-    }
-
-    refreshRequest() {
+  async componentWillMount() {
+    try {
+      const value = await AsyncStorage.getItem('loginCredential');
+      if (value !== null) {
+        console.log(value, 'Storage Request');
+        this.setState({ tokenUser: value })
         return this.getData();
+      }
+    } catch (error) {
+      console.log(error, 'Error Storage Request');
     }
+  }
 
-    renderData = (item) => {
-        console.log(item, 'Data ReQ');
+  getData() {
+    axios.get(`${BASE_URL}/buyer/requests?key=to&page=0&pageSize=30&sorting=desc`, {
+      headers: {
+        'token': this.state.tokenUser
+      }
+    }).then(response => {
+      res = response.data.data;
+      console.log(res, 'Data Request Order');
+      this.setState({ dataReqOrder: res, loading: false });
+    })
+      .catch(error => {
+        this.setState({ loading: false });
+        console.log(error.response, 'Erroor nya');
+        console.log('Error Request Order Get Data');
+      })
+  }
 
-        return item.map((datax, index) => {
-            console.log(datax, 'Data Maping Request');
-            const dateFormat = moment(datax.expiredAt).format('DD/MM/YYYY');
-            const timeFormat = moment(datax.expiredAt).format('h:mm:ss');
-            if (datax.Status.id == 19) {
-                if (datax.sanggup == 0) {
-                    return (
-                        <Card>
-                            <View
-                                style={styles.itemContainerStyle}
-                            >
-                                <View style={styles.thumbnailContainerStyle}>
-                                    <Image
-                                        style={styles.thumbnailStyle}
-                                        source={{ uri: `${BASE_URL}/images/${datax.photo}` }}
-                                    />
-                                </View>
-                                <View style={styles.headerContentStyle}>
-                                    <Text style={styles.headerTextStyle}>{datax.Fish.name}</Text>
-                                    <View style={{ flexDirection: 'column', flex: 1 }}>
-                                        <Text style={{ fontSize: 13 }}>Batas Waktu: {dateFormat} Pukul: {timeFormat} </Text>
-                                        <Text style={{ color: 'red', fontWeight: 'bold' }}>Expired</Text>
-                                    </View>
-                                </View>
-                            </View>
-                        </Card>
-                    );
-                }
+  static navigationOptions = {
+    title: 'Permintaan',
+    headerRight: <View />
+  }
 
-                if (datax.sanggup > 0) {
-                    return (
-                        <TouchableWithoutFeedback
-                            onPress={() => this.detailOrder(datax)}
-                            key={datax.id}
-                        >
-                            <Card>
-                                <View style={styles.itemContainerStyle}>
-                                    <View style={styles.thumbnailContainerStyle}>
-                                        <Image
-                                            style={styles.thumbnailStyle}
-                                            source={{ uri: `${BASE_URL}/images/${datax.photo}` }}
-                                        />
-                                    </View>
-                                    <View style={styles.headerContentStyle}>
-                                        <Text style={styles.headerTextStyle}>{datax.Fish.name}</Text>
-                                        <View style={{ flexDirection: 'column', flex: 1 }}>
-                                            <Text style={{ fontSize: 13 }}>Batas Waktu: {dateFormat} Pukul: {timeFormat} </Text>
-                                            <Text>{datax.sanggup} Sanggup | {datax.tidakSanggup} Menolak | {datax.menunggu} Menunggu</Text>
-                                        </View>
-                                    </View>
-                                </View>
-                            </Card>
-                        </TouchableWithoutFeedback>
-                    );
-                }
-            }
+  refreshRequest() {
+    return this.getData();
+  }
 
-            if (datax.Status.id === 20) {
-                return (
-                    <TouchableWithoutFeedback
-                        onPress={() => this.detailOrder(datax)}
-                        key={datax.id}
-                    >
-                        <Card>
-                            <View style={styles.itemContainerStyle}>
-                                <View style={styles.thumbnailContainerStyle}>
-                                    <Image
-                                        style={styles.thumbnailStyle}
-                                        source={{ uri: `${BASE_URL}/images/${datax.photo}` }}
-                                    />
-                                </View>
-                                <View style={styles.headerContentStyle}>
-                                    <Text style={styles.headerTextStyle}>{datax.Fish.name}</Text>
-                                    <View style={{ flexDirection: 'column', flex: 1 }}>
-                                        <Text style={{ fontSize: 13 }}>Batas Waktu: {dateFormat} Pukul: {timeFormat} </Text>
-                                        <Text>{datax.sanggup} Sanggup | {datax.tidakSanggup} Menolak | {datax.menunggu} Menunggu</Text>
-                                    </View>
-                                </View>
-                            </View>
-                        </Card>
-                    </TouchableWithoutFeedback>
-                );
-            }
-        })
-    }
+  renderData = (item) => {
+    console.log(item, 'Data ReQ');
 
-    renderFlatList = () => {
-        return (
-            <View>
-                <FlatList
-                    data={[this.state.dataReqOrder]}
-                    renderItem={({ item }) => this.renderData(item)}
-                    keyExtractor={(item, index) => index}
-                />
-            </View>
-        );
-    }
-
-
-    detailOrder = (props) => {
-        const listData = props;
-        this.props.navi.navigate('DetailRequestOrder', { datas: listData })
-    }
-
-
-    render() {
-        if (this.state.loading) {
-            return <Spinner size="large" />
-        }
-        return (
-            <ScrollView>
-                <View>
-                    {this.renderFlatList()}
+    return item.map((datax, index) => {
+      console.log(datax, 'Data Maping Request');
+      const dateFormat = moment(datax.expiredAt).format('DD/MM/YYYY');
+      const timeFormat = moment(datax.expiredAt).format('h:mm:ss');
+      if (datax.Status.id == 19) {
+        if (datax.sanggup == 0) {
+          return (
+            <Card>
+              <View
+                style={styles.itemContainerStyle}
+              >
+                <View style={styles.thumbnailContainerStyle}>
+                  <Image
+                    style={styles.thumbnailStyle}
+                    source={{ uri: `${BASE_URL}/images/${datax.photo}` }}
+                  />
                 </View>
-            </ScrollView>
+                <View style={styles.headerContentStyle}>
+                  <Text style={styles.headerTextStyle}>{datax.Fish.name}</Text>
+                  <View style={{ flexDirection: 'column', flex: 1 }}>
+                    <Text style={{ fontSize: 13 }}>Batas Waktu: {dateFormat} Pukul: {timeFormat} </Text>
+                    <Text style={{ color: 'red', fontWeight: 'bold' }}>Expired</Text>
+                  </View>
+                </View>
+              </View>
+            </Card>
+          );
+        }
+
+        if (datax.sanggup > 0) {
+          return (
+           
+            <Card>
+              <TouchableWithoutFeedback
+                onPress={() => this.detailOrder(datax)}
+                key={datax.id}
+              >
+                <View style={styles.itemContainerStyle}>
+                  <View style={styles.thumbnailContainerStyle}>
+                    <Image
+                      style={styles.thumbnailStyle}
+                      source={{ uri: `${BASE_URL}/images/${datax.photo}` }}
+                    />
+                  </View>
+                  <View style={styles.headerContentStyle}>
+                    <Text style={styles.headerTextStyle}>{datax.Fish.name}</Text>
+                    <View style={{ flexDirection: 'column', flex: 1 }}>
+                      <Text style={{ fontSize: 13 }}>Batas Waktu: {dateFormat} Pukul: {timeFormat} </Text>
+                      <Text>{datax.sanggup} Sanggup | {datax.tidakSanggup} Menolak | {datax.menunggu} Menunggu</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            </Card>
+          );
+        }
+      }
+
+      if (datax.Status.id === 20) {
+        return (
+          <Card>
+            <TouchableWithoutFeedback
+              onPress={() => this.detailOrder(datax)}
+              key={datax.id}
+            >
+              <View style={styles.itemContainerStyle}>
+                <View style={styles.thumbnailContainerStyle}>
+                  <Image
+                    style={styles.thumbnailStyle}
+                    source={{ uri: `${BASE_URL}/images/${datax.photo}` }}
+                  />
+                </View>
+                <View style={styles.headerContentStyle}>
+                  <Text style={styles.headerTextStyle}>{datax.Fish.name}</Text>
+                  <View style={{ flexDirection: 'column', flex: 1 }}>
+                    <Text style={{ fontSize: 13 }}>Batas Waktu: {dateFormat} Pukul: {timeFormat} </Text>
+                    <Text>{datax.sanggup} Sanggup | {datax.tidakSanggup} Menolak | {datax.menunggu} Menunggu</Text>
+                  </View>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </Card>
         );
+      }
+    })
+  }
+
+  renderFlatList = () => {
+    return (
+      <View>
+        <FlatList
+          data={[this.state.dataReqOrder]}
+          renderItem={({ item }) => this.renderData(item)}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
+  }
+
+
+  detailOrder = (props) => {
+    const listData = props;
+    this.props.navi.navigate('DetailRequestOrder', { datas: listData })
+  }
+
+
+  render() {
+    if (this.state.loading) {
+      return <Spinner size="large" />
     }
+    return (
+      <ScrollView>
+        <View>
+          {this.renderFlatList()}
+        </View>
+      </ScrollView>
+    );
+  }
 };
 
 const styles = {
-    itemContainerStyle: {
-        padding: 5,
-        justifyContent: 'flex-start',
-        flexDirection: 'row'
-    },
-    thumbnailContainerStyle: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 15,
-    },
-    thumbnailStyle: {
-        // alignSelf: 'stretch',
-        height: 50,
-        width: 50,
-        borderWidth: 1,
-        borderRadius: 100,
-        // resizeMode: 'cover'
-    },
-    headerContentStyle: {
-        flex: 1,
-        marginRight: 15,
-        marginTop: 5,
-        marginBottom: 10,
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 20,
-        color: 'black',
-        fontWeight: 'bold'
-    },
-    titleTextStyle: {
-        fontSize: 15,
-        fontWeight: 'bold'
-    }
+  itemContainerStyle: {
+    padding: 5,
+    justifyContent: 'flex-start',
+    flexDirection: 'row'
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+  },
+  thumbnailStyle: {
+    // alignSelf: 'stretch',
+    height: 50,
+    width: 50,
+    borderWidth: 1,
+    borderRadius: 100,
+    // resizeMode: 'cover'
+  },
+  headerContentStyle: {
+    flex: 1,
+    marginRight: 15,
+    marginTop: 5,
+    marginBottom: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  titleTextStyle: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
 }
 
 export default RequestOrderPage;
--- src/pages/TransactionPage.js
+++ src/pages/TransactionPage.js
@@ -1,212 +1,213 @@
 import React, { Component } from 'react';
 import {
-    Text,
-    View,
-    AsyncStorage,
-    FlatList,
-    Image,
-    TouchableWithoutFeedback,
-    ScrollView
+  Text,
+  View,
+  AsyncStorage,
+  FlatList,
+  Image,
+  TouchableWithoutFeedback,
+  ScrollView
 } from 'react-native';
 import { Header, SearchBar, Icon } from 'react-native-elements';
 import axios from 'axios';
 import { BASE_URL } from './../shared/lb.config';
 import {
-    CardRegistration,
-    CardSectionRegistration,
-    InputRegistration,
-    ContainerSection,
-    Container,
-    Spinner,
-    Button,
-    Card
+  CardRegistration,
+  CardSectionRegistration,
+  InputRegistration,
+  ContainerSection,
+  Container,
+  Spinner,
+  Button,
+  Card
 } from './../components/common';
 import moment from 'moment';
 
 class TransactionPage extends Component {
-    static navigationOptions = {
-        title: 'Transaksi',
-        headerRight: <View />
-    }
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            tokenUser: '',
-            dataTransaksi: []
-        };
+  static navigationOptions = {
+    title: 'Transaksi',
+    headerRight: <View />
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      tokenUser: '',
+      dataTransaksi: []
     };
+  };
 
 
-    async componentWillMount() {
-        try {
-            const value = await AsyncStorage.getItem('loginCredential');
-            if (value !== null) {
-                console.log(value, 'Storage Request');
-                this.setState({ tokenUser: value })
-                return this.getData();
-            }
-        } catch (error) {
-            console.log(error, 'Error Storage Request');
-        }
-    }
-
-    getData() {
-        console.log('API FIRE!')
-        axios.get(`${BASE_URL}/buyer/orders?page=0&pageSize=50&sorting=DESC`, {
-            headers: {
-                'token': this.state.tokenUser
-            }
-        }).then(response => {
-            const result = response.data.data;
-            console.log(response, 'Data Transaksi');
-            this.setState({ dataTransaksi: result, loading: false });
-        })
-            .catch(error => {
-                this.setState({ loading: false });
-                console.log(error.response, 'Error nya');
-                console.log('Error Transation Get Data');
-            })
-    }
-
-    detailTransaction = (props) => {
-        const dataTransaction = props;
-        this.props.navi.navigate('DetailTransaction', { datas: dataTransaction })
-    }
-
-    refreshRequest() {
+  async componentWillMount() {
+    try {
+      const value = await AsyncStorage.getItem('loginCredential');
+      if (value !== null) {
+        console.log(value, 'Storage Request');
+        this.setState({ tokenUser: value })
         return this.getData();
+      }
+    } catch (error) {
+      console.log(error, 'Error Storage Request');
     }
+  }
 
-    renderData = (item) => {
-        console.log(item, 'Data Trans');
-        if (item.length === 0) {
-            return (
-                <View style={{ paddingTop: '50%' }}>
-                    <Text style={{ color: 'grey', paddingLeft: '25%' }}>
-                        Ups.. Your connection internet to slow!
-                    </Text>
-                    <Button onPress={() => this.refreshRequest()}>
-                        Tap Tap Me Please!
-                    </Button>
-                </View>
-            );
-        }
-        return item.map((item, index) => {
-            const dateFormat = moment(item.Request.Transaction.updatedAt).format('DD/MM/YYYY');
-            const timeFormat = moment(item.Request.Transaction.updatedAt).format('h:mm:ss');
-            return (
-                <TouchableWithoutFeedback
-                    key={item.id}
-                    onPress={() => this.detailTransaction(item)}
-                >
-                    <Card>
-                        <View style={styles.itemContainerStyle}>
-                            <View style={styles.thumbnailContainerStyle}>
-                                <Image
-                                    style={styles.thumbnailStyle}
-                                    source={{ uri: `${BASE_URL}/images/${item.Request.Transaction.photo}` }}
-                                />
-                            </View>
-                            <View style={{ flexDirection: 'column', flex: 1 }}>
-                                <View>
-                                    <Text style={styles.headerTextStyle}>{item.Request.Transaction.Fish.name} - {item.Request.Transaction.quantity} Kg</Text>
-                                    <Text style={styles.titleTextStyle}>{item.Request.Supplier.name}</Text>
-                                </View>
+  getData() {
+    console.log('API FIRE!')
+    axios.get(`${BASE_URL}/buyer/orders?page=0&pageSize=50&sorting=DESC`, {
+      headers: {
+        'token': this.state.tokenUser
+      }
+    }).then(response => {
+      const result = response.data.data;
+      console.log(response, 'Data Transaksi');
+      this.setState({ dataTransaksi: result, loading: false });
+    })
+      .catch(error => {
+        this.setState({ loading: false });
+        console.log(error.response, 'Error nya');
+        console.log('Error Transation Get Data');
+      })
+  }
 
-                                <View style={{ flexDirection: 'row', flex: 1 }}>
-                                    <Image
-                                        style={styles.trackingImage}
-                                        source={require('./../assets/image/ts1.png')}
-                                    />
-                                    <Image
-                                        style={styles.trackingImage}
-                                        source={require('./../assets/image/ts2.png')}
-                                    />
-                                    <Image
-                                        style={styles.trackingImage}
-                                        source={require('./../assets/image/ts3.png')}
-                                    />
-                                    <Image
-                                        style={styles.trackingImage}
-                                        source={require('./../assets/image/ts4.png')}
-                                    />
-                                    <Image
-                                        style={styles.trackingImage}
-                                        source={require('./../assets/image/ts5.png')}
-                                    />
-                                </View>
-                            </View>
+  detailTransaction = (props) => {
+    const dataTransaction = props;
+    this.props.navi.navigate('DetailTransaction', { datas: dataTransaction })
+  }
 
-                            <View>
-                                <Text>DP Dibayar</Text>
-                                <Text>{dateFormat}</Text>
-                            </View>
-                        </View>
-                    </Card>
-                </TouchableWithoutFeedback>
-            )
-        })
+  refreshRequest() {
+    return this.getData();
+  }
+
+  renderData = (item) => {
+    console.log(item, 'Data Trans');
+    if (item.length === 0) {
+      return (
+        <View style={{ paddingTop: '50%' }}>
+          <Text style={{ color: 'grey', paddingLeft: '25%' }}>
+            Ups.. Your connection internet to slow!
+          </Text>
+          <Button onPress={() => this.refreshRequest()}>
+            Tap Tap Me Please!
+          </Button>
+        </View>
+      );
     }
-
-
-    render() {
-        if (this.state.loading) {
-            return <Spinner size="large" />
-        }
-        return (
-            <ScrollView>
+    return item.map((item, index) => {
+      const dateFormat = moment(item.Request.Transaction.updatedAt).format('DD/MM/YYYY');
+      const timeFormat = moment(item.Request.Transaction.updatedAt).format('h:mm:ss');
+      return (
+        
+        <Card>
+          <TouchableWithoutFeedback
+            key={item.id}
+            onPress={() => this.detailTransaction(item)}
+          >
+            <View style={styles.itemContainerStyle}>
+              <View style={styles.thumbnailContainerStyle}>
+                <Image
+                  style={styles.thumbnailStyle}
+                  source={{ uri: `${BASE_URL}/images/${item.Request.Transaction.photo}` }}
+                />
+              </View>
+              <View style={{ flexDirection: 'column', flex: 1 }}>
                 <View>
-                    <FlatList
-                        data={[this.state.dataTransaksi]}
-                        renderItem={({ item }) => this.renderData(item)}
-                    />
+                  <Text style={styles.headerTextStyle}>{item.Request.Transaction.Fish.name} - {item.Request.Transaction.quantity} Kg</Text>
+                  <Text style={styles.titleTextStyle}>{item.Request.Supplier.name}</Text>
                 </View>
-            </ScrollView>
-        );
+
+                <View style={{ flexDirection: 'row', flex: 1 }}>
+                  <Image
+                    style={styles.trackingImage}
+                    source={require('./../assets/image/ts1.png')}
+                  />
+                  <Image
+                    style={styles.trackingImage}
+                    source={require('./../assets/image/ts2.png')}
+                  />
+                  <Image
+                    style={styles.trackingImage}
+                    source={require('./../assets/image/ts3.png')}
+                  />
+                  <Image
+                    style={styles.trackingImage}
+                    source={require('./../assets/image/ts4.png')}
+                  />
+                  <Image
+                    style={styles.trackingImage}
+                    source={require('./../assets/image/ts5.png')}
+                  />
+                </View>
+              </View>
+
+              <View>
+                <Text>DP Dibayar</Text>
+                <Text>{dateFormat}</Text>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </Card>
+      )
+    })
+  }
+
+
+  render() {
+    if (this.state.loading) {
+      return <Spinner size="large" />
     }
+    return (
+      <ScrollView>
+        <View>
+          <FlatList
+            data={[this.state.dataTransaksi]}
+            renderItem={({ item }) => this.renderData(item)}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
 };
 
 
 
 const styles = {
-    thumbnailStyle: {
-        height: 50,
-        width: 50,
-        borderRadius: 8
-    },
-    itemContainerStyle: {
-        padding: 5,
-        justifyContent: 'flex-start',
-        flexDirection: 'row'
-    },
-    thumbnailContainerStyle: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 15,
-    },
-    trackingImage: {
-        height: 22,
-        width: 22,
-        borderRadius: 8
-    },
-    headerContentStyle: {
-        flex: 1,
-        marginRight: 15,
-        marginTop: 5,
-        marginBottom: 10,
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 15,
-        color: 'black',
-        fontWeight: 'bold'
-    },
-    titleTextStyle: {
-        fontSize: 13,
-        fontWeight: 'bold'
-    }
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+    borderRadius: 8
+  },
+  itemContainerStyle: {
+    padding: 5,
+    justifyContent: 'flex-start',
+    flexDirection: 'row'
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+  },
+  trackingImage: {
+    height: 22,
+    width: 22,
+    borderRadius: 8
+  },
+  headerContentStyle: {
+    flex: 1,
+    marginRight: 15,
+    marginTop: 5,
+    marginBottom: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  titleTextStyle: {
+    fontSize: 13,
+    fontWeight: 'bold'
+  }
 }
 
 export default TransactionPage;
