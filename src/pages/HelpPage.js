594128ed555824a048462f67ec6534324db83368
REN <rendy1429@gmail.com>
2018-04-23 14:24:24 +0700

    add button to contact us redirect to chat admin

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { View, Text, Image, Linking, TouchableOpacity } from 'react-native'
 import Config from 'react-native-config'
 
-import { Container } from '../components/common'
+import { Container, Button, ContainerSection } from '../components/common'
 
 class HelpPage extends Component {
 	static navigationOptions = {
@@ -10,26 +10,31 @@ class HelpPage extends Component {
 		headerRight: <View />
 	}
 
+	redirectDiskusiAdmin = () => {
+		console.log('Chat Admin');
+		this.props.navigation.navigate('MessageAdmin');
+	}
+
 	render() {
 		return (
-			<View style={{flex: 1, marginTop: 50}}>
+			<View style={{ flex: 1, marginTop: 50 }}>
 				<Container>
 					<View>
 						<Image
-							style={{alignSelf: 'center'}}
-							source={require('./../assets/images/logo.png')} 
+							style={{ alignSelf: 'center' }}
+							source={require('./../assets/images/logo.png')}
 						/>
 					</View>
-					<Text style={{marginBottom: 30, marginTop: 10, textAlign: 'center'}}>v{Config.VERSION_NAME} - build {Config.BUILD_TYPE}</Text>
+					<Text style={{ marginBottom: 30, marginTop: 10, textAlign: 'center' }}>v{Config.VERSION_NAME} - build {Config.BUILD_TYPE}</Text>
 
-					<View style={{justifyContent: 'center', alignItems: 'center'}}>
+					<View style={{ justifyContent: 'center', alignItems: 'center' }}>
 						<Text>Hubungi</Text>
 						<Text>Pusat Bantuan</Text>
 						<TouchableOpacity
 							onPress={() => Linking.openURL('tel:0217778888').catch(err => console.error('An error occurred', err))}
 						>
 							<View>
-								<Text style={{fontSize: 20, marginTop: 10, marginBottom: 10}}>
+								<Text style={{ fontSize: 20, marginTop: 10, marginBottom: 10 }}>
 									+62 21 777 8888
 								</Text>
 							</View>
@@ -40,11 +45,17 @@ class HelpPage extends Component {
 							onPress={() => Linking.openURL('mailto:info@aruna.id').catch(err => console.error('An error occurred', err))}
 						>
 							<View>
-								<Text style={{fontSize: 20, marginTop: 10, marginBottom: 10}}>info@aruna.id</Text>
+								<Text style={{ fontSize: 20, marginTop: 10, marginBottom: 10 }}>info@aruna.id</Text>
 							</View>
 						</TouchableOpacity>
 
 					</View>
+
+					<ContainerSection>
+						<Button onPress={() => { this.redirectDiskusiAdmin() }}>
+							Chat Admin
+						</Button>
+					</ContainerSection>
 				</Container>
 			</View>
 		)
